e59ee470f17e8dbfce42765033a126d655d71bc2
Pierre-Olivier Quirion <pioliqui@gmail.com>
2015-07-26 00:15:45 -0400

    add docker support in cfg

--- niak_server/niak_server/cfg.py
+++ niak_server/niak_server/cfg.py
@@ -1,12 +1,13 @@
+import os
 
-PROJECT_ROOT = '/home/poquirion/simexp/web_niak/niak_server/niak_server/'
+
+PROJECT_ROOT = os.getenv("PROJECT_ROOT_IN_DOCKER") \
+    if os.getenv("PROJECT_ROOT_IN_DOCKER") else '/home/poquirion/simexp/web_niak/niak_server/niak_server/'
 
 DATA_PATH = "data/"
 
 TEMPLATE_PATH = "templates/"
 
 STATIC_PATH = "static/"
-
-# DATA_PATH = "/home/poquirion/agrile/niak"
 
 
